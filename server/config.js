86356a666cdfd0ec7094b0b71545cde49b84f7c8
Artem Medeusheyev <artem.medeu@gmail.com>
2013-11-18 11:56:07 +0600

    server: use ‘path’ in config

--- server/config.js
+++ server/config.js
@@ -1,11 +1,17 @@
+var path = require('path');
+
+function mkpath (p) {
+  return path.resolve(__dirname + p);
+}
+
 module.exports = {
   server: {
     port: process.env.PORT || 3000,
     cookieSecret: 'trello-lite-client-secret'
   },
   client: {
-    dist: __dirname + '/../client/dist',
-    partials: __dirname + '/../client/dist/partials'
+    dist: mkpath('/../client/dist'),
+    partials: mkpath('/../client/dist/partials')
   },
   trello: {
     apiKey: process.env.API_KEY || 'someKey',
